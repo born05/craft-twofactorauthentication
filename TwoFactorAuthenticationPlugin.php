f68ab6e2ea324e07fb90ad0a5a101b18903683e3
Roel van Hintum <roel@born05.com>
2017-05-05 11:54:05 +0200

    Bump version to 0.0.3

--- TwoFactorAuthenticationPlugin.php
+++ TwoFactorAuthenticationPlugin.php
@@ -10,7 +10,7 @@ class TwoFactorAuthenticationPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '0.0.2';
+        return '0.0.3';
     }
 
     public function getSchemaVersion()
